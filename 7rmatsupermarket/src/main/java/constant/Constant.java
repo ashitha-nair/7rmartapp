faae27c7c85cc16051ac34e317a3ecd0a1a581da
ASUS <ASUS@DESKTOP-IO3GIFB>
2025-10-13 22:15:34 +0530

    constant file

---
+++ 7rmatsupermarket/src/main/java/constant/Constant.java
@@ -0,0 +1,15 @@
+package constant;
+
+public class Constant {
+
+	//copy the excel path in a variable
+	
+	public static final String TESTDATA=System.getProperty("user.dir")+"\\src\\test\\resources\\TestData.xlsx";
+	//system- class, getPtoprty- method,user.dir -directory
+	public static final String TESTIMAGE=System.getProperty("user.dir")+"\\src\\test\\resources\\mango.jpg";
+	
+	public static final String CONFIGFILE= System.getProperty("user.dir")+"\\src\\main\\resources\\config.properties";
+	
+	public static final String LOGINVALIDCREDENTIALS= "unable to load the page after entering valid username and password";
+		
+}
